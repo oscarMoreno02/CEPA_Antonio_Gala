7268099c5428809906ad2d563ad43689a188349e
Raul GM <loscanos3raul@gmail.com>
2024-01-31 09:25:43 +0100

    rutas, model y controler de usuario

---
+++ back/models/usuario.js
@@ -0,0 +1,11 @@
+
+class usuario{
+    constructor(id, nombre, email, password){
+        this.id = id
+        this.nombre = nombre
+        this.email = email
+        this.password = password
+    }
+}
+
+module.exports = usuario
