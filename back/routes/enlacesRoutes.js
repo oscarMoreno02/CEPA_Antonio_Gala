27885fa49f958b1c2cc46500365db23edad8cddb
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2024-02-02 14:09:23 +0100

    enlaces rutas con validator

--- back/routes/enlacesRoutes.js
+++ back/routes/enlacesRoutes.js
@@ -3,16 +3,30 @@ const {Router} = require('express');
 const router = Router();
 const { check } = require('express-validator');
 const {validateValues}=require('../helpers/validar-campos')
-
+const validator=require('../helpers/enlaces-validators')
 // const midCategoria=require('../middlewares/categoriasMid')
 const controller=require('../controllers/enlacesController')
 
     router.get('/seccion/:id',controller.listEnlacesBySeccion)
     router.get('/:id',controller.listEnlace)
     router.get('',controller.listAllEnlaces)
-    router.put('/:id',controller.editEnlace)
     router.delete('/:id',controller.removeEnlace)
-
-    router.post('',controller.createEnlace)
+    
+    router.put('/:id',
+    [
+        check('textClave','Tamaño del texto clave incorrecto ').trim().isLength({ min: 5 }),
+        check('url','Formato de enlace incorrecto').isURL(),
+        check('idSeccion').custom(validator.seccionExiste),
+        validateValues
+    ],
+    controller.editEnlace)
+    router.post('',
+    [
+        check('textClave','Tamaño del texto clave incorrecto ').trim().isLength({ min: 5 }),
+        check('url','Formato de enlace incorrecto').isURL(),
+        check('idSeccion').custom(validator.seccionExiste),
+        validateValues
+    ],
+    controller.createEnlace)
 
     module.exports = router;
