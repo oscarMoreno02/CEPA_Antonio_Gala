2dd1cee7c815e0b0a89ed989dea0a1e81b5e5fb0
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2024-02-05 20:02:15 +0100

    creacion de categorias

--- front/src/app/services/categorias.service.ts
+++ front/src/app/services/categorias.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable, catchError, of } from 'rxjs';
+import { Categoria } from '../interface/categoria';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
+  baseUrl=environment.baseUrl+environment.urlCategorias
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getAllCategoriasAgrupadas(): Observable<any | undefined> {
+    return this.http.get<any>(this.baseUrl+'/agrupadas').pipe(
+
+      catchError((error) =>{
+        return of(undefined)
+      })
+    )
+  }
+  getAllCategorias(): Observable<any | undefined> {
+    return this.http.get<any>(this.baseUrl).pipe(
+      catchError((error) =>{
+        return of(undefined)
+      })
+    )
+  }
+  insertCategoria(categoria:Categoria): Observable<any | undefined> {
+    let body={categoria:categoria}
+  
+     return this.http.post<any>(this.baseUrl,categoria).pipe(
+     
+     )
+   }
 }
