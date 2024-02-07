e50f1f4d37242d512dc9c9c06edd5ad982cf3ca0
Raul GM <loscanos3raul@gmail.com>
2024-02-07 23:11:54 +0100

    empezando a mostrar, añadir y editar los usuarios

---
+++ front/src/app/components/editar-usuarios/editar-usuarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-usuarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-usuarios.component.html',
+  styleUrl: './editar-usuarios.component.css'
+})
+export class EditarUsuariosComponent {
+
+}
