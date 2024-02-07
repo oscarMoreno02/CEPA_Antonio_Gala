ba79a1b1f4844cbd862ac5b35dee279f26272405
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2024-02-07 12:20:13 +0100

    listar noticias admin

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AdminCategoriasComponent } from './components/admin-categorias/admin-categorias.component';
 import { NoticiasCategoriaComponent } from './components/noticias-categoria/noticias-categoria.component';
 import { EditNoticiaComponent } from './components/edit-noticia/edit-noticia.component';
+import { AdminNoticiaComponent } from './components/admin-noticia/admin-noticia.component';
 
 export const routes: Routes = [
 {path:'admin/categorias',component: AdminCategoriasComponent},
 {path:'categoria/:id',component: NoticiasCategoriaComponent},
 {path:'categorias/:id/noticia/:noticia',component: NoticiasCategoriaComponent},
 {path:'noticia/:id',component: EditNoticiaComponent},
+{path:'admin/noticias',component: AdminNoticiaComponent},
 ];
---
+++ front/src/app/components/admin-noticia/admin-noticia.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NuevaNoticiaComponent } from '../nueva-noticia/nueva-noticia.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterLink } from '@angular/router';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { NoticiaService } from '../../services/noticia.service';
+import { Subscription } from 'rxjs';
+import { Noticia } from '../../interface/noticia';
+import { CategoriasService } from '../../services/categorias.service';
+import { Categoria } from '../../interface/categoria';
+
+@Component({
+  selector: 'app-admin-noticia',
+  standalone: true,
+  imports: [
+    NuevaNoticiaComponent,
+    HttpClientModule,
+    RouterLink,
+    TableModule,
+    ButtonModule,],
+    providers:[NoticiaService, CategoriasService],
+  templateUrl: './admin-noticia.component.html',
+  styleUrl: './admin-noticia.component.css',
+  
+})
+export class AdminNoticiaComponent implements OnInit {
+  constructor(
+    private servicioNoticia:NoticiaService,
+    private router: Router,
+    private servicioCategoria:CategoriasService
+  ){}
+  subscripcionNoticia: Subscription=new Subscription;
+  listaNoticias:Array<Noticia>=[]
+  listaCategorias:Array<Categoria>=[]
+    @Input() admin=true
+  
+  ngOnInit(): void {
+
+    this.subscripcionNoticia = this.servicioNoticia.getAllNoticias().subscribe({
+      next: (data: Array<Noticia>) => {
+        this.listaNoticias=data
+        this.servicioCategoria.getAllCategorias().subscribe({
+          next:(n:Array<Categoria>)=>{
+            this.listaCategorias=n
+
+          },
+          error:(err)=>{
+            console.log(err);
+          }
+        })
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+}
