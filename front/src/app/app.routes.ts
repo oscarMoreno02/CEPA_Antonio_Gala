f7151a99897d6655a5c271137048a5d6d8838404
Laura Pedraza <laurampego@gmail.com>
2024-02-08 08:36:01 +0100

    Arreglo de mostrar los eventos FRONT

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AdminCategoriasComponent } from './components/admin-categorias/admin-categorias.component';
+import { EventosComponent } from './eventos/eventos.component';
 
 export const routes: Routes = [
-{path:'admin/categorias',component:AdminCategoriasComponent}
+{path:'admin/categorias',component:AdminCategoriasComponent},
+{path:'eventos/mostrarEventos', component:EventosComponent}
 ];
--- front/src/environments/environment.development.ts
+++ front/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     baseUrl:'http://localhost:9090/api',
     urlCategorias:'/categorias',
-    urlEventos: '/eventos',
+    urlEventos: '/evento',
 };
