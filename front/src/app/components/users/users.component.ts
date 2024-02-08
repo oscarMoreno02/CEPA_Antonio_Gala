6c62ce153e39061765864f77ebcd02e7c3216ca7
Raul GM <loscanos3raul@gmail.com>
2024-02-08 20:32:55 +0100

    Avance tarde 08/02
    Creación de UsersService, la interfaz users y conexión con el backend

--- front/src/app/components/users/users.component.ts
+++ front/src/app/components/users/users.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { EditableColumn, TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { NuevoUsuariosComponent } from '../nuevo-usuarios/nuevo-usuarios.component';
+import { EditarUsuariosComponent } from '../editar-usuarios/editar-usuarios.component';
+import { UsersService } from '../../services/users.service';
+import { Users } from '../../interface/users';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,
+    RouterLink,
+    TableModule,
+    ButtonModule,
+    NuevoUsuariosComponent,
+    EditarUsuariosComponent],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
+  providers: [UsersService]
 })
 export class UsersComponent {
-
+  
 }
