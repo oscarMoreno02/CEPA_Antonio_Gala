fc65b07a0ccbe40f1e2cf8e1edec98390b0e7f1e
Laura Pedraza <laurampego@gmail.com>
2024-02-15 11:15:12 +0100

    Validar post eventos

--- back/routes/eventoRoutes.js
+++ back/routes/eventoRoutes.js
@@ -1,18 +1,19 @@
 // Laura
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 const controller = require('../controllers/eventoController');
 
 router.get('/obtenerEventos', controller.obtenerEventos);
 router.get('/eventos/:id', controller.obtenerEventoPorId);
 router.post('/eventos', [
-    check('nombre'),
-    check('descripcion'),
-    check('fecha'),
-    check('hora'),
-    check('foto'),
-    check('mg'),
-    check('visibilidad')
+    check('nombre').isString().notEmpty().isLength({ min: 2, max: 50 }),
+    check('descripcion').isString().notEmpty(),
+    check('fecha').isString().notEmpty(),
+    check('hora').isString().notEmpty(),
+    check('fotoCartel').isString().notEmpty(),
+    check('mg').isInt(),
+    check('visibilidad').isBoolean().notEmpty(),
 ], controller.subirEvento);
 router.delete('/eventos/:id', controller.borrarEvento);
 router.put('/eventos/:id', controller.actualizarEvento);
