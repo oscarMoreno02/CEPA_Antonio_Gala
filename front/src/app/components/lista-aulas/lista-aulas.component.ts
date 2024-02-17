239a10e3488c619a0233242b9d8eb8f5120e7b1b
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2024-02-17 19:04:26 +0100

    cabecera jefe estudios

---
+++ front/src/app/components/lista-aulas/lista-aulas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-aulas',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-aulas.component.html',
+  styleUrl: './lista-aulas.component.css'
+})
+export class ListaAulasComponent {
+
+}
