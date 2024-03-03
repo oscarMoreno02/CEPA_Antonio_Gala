b46411e2c3239bfccedb855e27e3369548bd986a
Raul GM <loscanos3raul@gmail.com>
2024-03-03 21:08:20 +0100

    avances en el component

--- front/src/app/components/modificar-roles/modificar-roles.component.ts
+++ front/src/app/components/modificar-roles/modificar-roles.component.ts
@@ -1,21 +1,44 @@
 import { Component, EventEmitter ,Input, OnInit, Output } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { DialogModule } from 'primeng/dialog';
+import { DialogService} from 'primeng/dynamicdialog';
 import { ToastModule } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ConfirmComponent } from '../confirm/confirm.component';
+import { Subscription } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { RolAsignadoService } from '../../services/rol-asignado.service';
+import { RolAsignado } from '../../interface/rolAsignado';
 
 @Component({
   selector: 'app-modificar-roles',
   standalone: true,
   imports: [
+    FormsModule,
     ToastModule,
     ButtonModule,
-    DialogModule
+    DialogModule,
+    InputTextModule,
+    ConfirmComponent
   ],
   templateUrl: './modificar-roles.component.html',
-  styleUrl: './modificar-roles.component.css'
+  styleUrl: './modificar-roles.component.css',
+  providers:[DialogService,MessageService,RolAsignadoService]
 })
 export class ModificarRolesComponent {
 
+  constructor(
+    private servicioRolAsig:RolAsignadoService
+  ){}
+
+  @Input() idUser!: number
+  ro! : RolAsignado
+  rolAsignado : RolAsignado = {
+    id:0,
+    idUser: 0,
+    idRol: 0
+  }
   @Output() cerrarModal = new EventEmitter<void>();
   
   @Input() visible: boolean = false
@@ -27,7 +50,23 @@ export class ModificarRolesComponent {
   @Input() esProfe: boolean = false
 
   showDialog(){
+    this.servicioRolAsig.rolesAsignadosGetIdUsu(this.idUser!).subscribe({
+      
+      next: (rolA:RolAsignado) => {
+        //this.ro = rolA
+        this.visible=true
+        //this.rolAsignado.idRol = rolA.idRol
+      },
+      error: (e) => {
+        console.log(e)
+      }
+    })
+  }
 
+  async guardar(b:Boolean){
+    if(b){
+
+    }
   }
 
   cerrar(): void {
