ab82a5e5e574d7e5214fe0dc5087835a001a3973
Jaime <jaime.ornu@gmail.com>
2024-03-05 18:53:21 +0100

    Protegidas las rutas de aulas

--- back/routes/aulaEspecialRoutes.js
+++ back/routes/aulaEspecialRoutes.js
@@ -3,12 +3,27 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/aulaEspecialController')
+const { check } = require('express-validator')
+const { validateValues } = require('../helpers/validar-campos')
+const authMid = require('../middlewares/validarJWT')
+const accessMid = require('../middlewares/validarRoles')
+const aulaEspecialMid = require('../middlewares/aulaEspecialMid')
 
 router.get('/', controller.listAllAulas)
 router.get('/:id', controller.listAula)
 router.get('/data/:id', controller.listAulaWithData)
-router.post('/', controller.createAula)
-router.put('/:id', controller.editAula)
-router.delete('/:id', controller.removeAula)
+
+router.post('/',
+[
+    check('nombre').isString().notEmpty().isLength({ min: 2, max: 50 }),
+    validateValues
+], authMid.validarJWT, accessMid.esJefeDeEstudios, aulaEspecialMid.nombreEnUsoAula, controller.createAula)
+
+router.put('/:id', [
+    check('nombre').isString().notEmpty().isLength({ min: 2, max: 50 }),
+    validateValues
+], authMid.validarJWT, accessMid.esJefeDeEstudios, aulaEspecialMid.nombreEnUsoAula, controller.editAula)
+
+router.delete('/:id', authMid.validarJWT, accessMid.esJefeDeEstudios, controller.removeAula)
 
 module.exports = router
