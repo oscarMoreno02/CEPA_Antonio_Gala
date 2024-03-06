e904b7091c34fde3d4a885a264ae03f91996195c
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2024-03-06 13:04:53 +0100

    perfil-> profile icon

--- front/src/app/components/perfil/perfil.component.ts
+++ front/src/app/components/perfil/perfil.component.ts
@@ -1,160 +1,161 @@
-//Raul
-import { Component, EventEmitter ,Input, OnInit, Output } from '@angular/core';
-import { MessageService } from 'primeng/api';
-import { DialogService} from 'primeng/dynamicdialog';
-import { ToastModule } from 'primeng/toast';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { ModificarRolesComponent } from '../modificar-roles/modificar-roles.component';
-import { DialogModule } from 'primeng/dialog';
-import { FormsModule } from '@angular/forms';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { ConfirmComponent } from '../confirm/confirm.component';
-import { UsersService } from '../../services/users.service';
-import { Subscription } from 'rxjs';
-import { Users } from '../../interface/users';
+// //Raul
+// import { Component, EventEmitter ,Input, OnInit, Output } from '@angular/core';
+// import { MessageService } from 'primeng/api';
+// import { DialogService} from 'primeng/dynamicdialog';
+// import { ToastModule } from 'primeng/toast';
+// import { ButtonModule } from 'primeng/button';
+// import { InputTextModule } from 'primeng/inputtext';
+// import { ModificarRolesComponent } from '../modificar-roles/modificar-roles.component';
+// import { DialogModule } from 'primeng/dialog';
+// import { FormsModule } from '@angular/forms';
+// import { InputSwitchModule } from 'primeng/inputswitch';
+// import { ConfirmComponent } from '../confirm/confirm.component';
+// import { UsersService } from '../../services/users.service';
+// import { Subscription } from 'rxjs';
+// import { Users } from '../../interface/users';
 
-@Component({
-  selector: 'app-perfil',
-  standalone: true,
-  imports: [
-    FormsModule,
-    ToastModule,
-    DialogModule,
-    ButtonModule,
-    InputTextModule,
-    InputSwitchModule,
-    ConfirmComponent,
-    ModificarRolesComponent
-  ],
-  templateUrl: './perfil.component.html',
-  styleUrl: './perfil.component.css',
-  providers: [DialogService, MessageService, UsersService]
-})
-export class PerfilComponent implements OnInit{
-  constructor(
-    public messageService: MessageService,
-    private servicioUsers: UsersService
-  ) { }
-  value = ''
-  us!:Users
-  usuarios: Users = { 
-    id: 0, 
-    nombre: '', 
-    email: '', 
-    password: '' 
-  }
-  @Input() usuario?: any
-  @Input() id!: number
-  subscripcionUsuarios: Subscription = new Subscription;
+// @Component({
+//   selector: 'app-perfil',
+//   standalone: true,
+//   imports: [
+//     FormsModule,
+//     ToastModule,
+//     DialogModule,
+//     ButtonModule,
+//     InputTextModule,
+//     InputSwitchModule,
+//     ConfirmComponent,
+//     ModificarRolesComponent,
 
-  @Input() visible: boolean = false
+//   ],
+//   templateUrl: './perfil.component.html',
+//   styleUrl: './perfil.component.css',
+//   providers: [DialogService, MessageService, UsersService]
+// })
+// export class PerfilComponent implements OnInit{
+//   constructor(
+//     public messageService: MessageService,
+//     private servicioUsers: UsersService
+//   ) { }
+//   value = ''
+//   us!:Users
+//   usuarios: Users = { 
+//     id: 0, 
+//     nombre: '', 
+//     email: '', 
+//     password: '' 
+//   }
+//   @Input() usuario?: any
+//   @Input() id!: number
+//   subscripcionUsuarios: Subscription = new Subscription;
 
-  @Output() cerrarModal = new EventEmitter<void>();
+//   @Input() visible: boolean = false
 
-  @Input() tipo=0
+//   @Output() cerrarModal = new EventEmitter<void>();
 
-  estiloValidacionNombre = ''
-  estiloValidacionCorreo = ''
-  estiloValidacionContrasena = ''
+//   @Input() tipo=0
 
-  validaciones():Boolean{
+//   estiloValidacionNombre = ''
+//   estiloValidacionCorreo = ''
+//   estiloValidacionContrasena = ''
 
-    let validado = true
-    let nombreRegex = /^(?=.{3,15}$)[A-ZÁÉÍÓÚ][a-zñáéíóú]+([\s-][A-ZÁÉÍÓÚ][a-zñáéíóú]+)?$/
-    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+//   validaciones():Boolean{
 
-    if (this.usuarios.nombre){
+//     let validado = true
+//     let nombreRegex = /^(?=.{3,15}$)[A-ZÁÉÍÓÚ][a-zñáéíóú]+([\s-][A-ZÁÉÍÓÚ][a-zñáéíóú]+)?$/
+//     let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
-      var nombrePrueba = nombreRegex.test(this.usuarios.nombre)
+//     if (this.usuarios.nombre){
+
+//       var nombrePrueba = nombreRegex.test(this.usuarios.nombre)
       
-      if (nombrePrueba){
-        this.estiloValidacionNombre
-      }else{
-        this.estiloValidacionNombre = 'ng-invalid ng-dirty'
-        validado = false
-        this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El nombre no puede contener caracteres especiales', life:3000})
+//       if (nombrePrueba){
+//         this.estiloValidacionNombre
+//       }else{
+//         this.estiloValidacionNombre = 'ng-invalid ng-dirty'
+//         validado = false
+//         this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El nombre no puede contener caracteres especiales', life:3000})
 
-      }
-    }else{
+//       }
+//     }else{
 
-      this.estiloValidacionNombre = 'ng-invalid ng-dirty'
-      validado = false
-      this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El nombre es obligatorio', life:3000})
+//       this.estiloValidacionNombre = 'ng-invalid ng-dirty'
+//       validado = false
+//       this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El nombre es obligatorio', life:3000})
 
-    }
+//     }
 
-    if (this.usuarios.email){
+//     if (this.usuarios.email){
 
-      var emailPrueba = emailRegex.test(this.usuarios.email)
+//       var emailPrueba = emailRegex.test(this.usuarios.email)
       
-      if (emailPrueba){
-        this.estiloValidacionCorreo
-      }else{
-        this.estiloValidacionCorreo = 'ng-invalid ng-dirty'
-        validado = false
-        this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El correo tiene que tener el @', life:3000})
+//       if (emailPrueba){
+//         this.estiloValidacionCorreo
+//       }else{
+//         this.estiloValidacionCorreo = 'ng-invalid ng-dirty'
+//         validado = false
+//         this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El correo tiene que tener el @', life:3000})
 
-      }
-    }else{
+//       }
+//     }else{
 
-      this.estiloValidacionCorreo = 'ng-invalid ng-dirty'
-      validado = false
-      this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El correo es obligatorio', life:3000})
+//       this.estiloValidacionCorreo = 'ng-invalid ng-dirty'
+//       validado = false
+//       this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El correo es obligatorio', life:3000})
 
-    }
-    return validado
-  }
+//     }
+//     return validado
+//   }
 
-  ngOnInit(): void {
+//   ngOnInit(): void {
 
-  }
+//   }
 
-  cerrar(): void {
-    this.cerrarModal.emit();
-  }
+//   cerrar(): void {
+//     this.cerrarModal.emit();
+//   }
 
-  async guardar(b:Boolean){
-    if(b){
-      if(this.validaciones()){
-         this.servicioUsers.usuariosPut(this.usuarios,this.id).subscribe({
-          next: (data:any)=> {
-            setTimeout(()=>{
-              this.messageService.add({severity:'success', summary:'Actualizar usuario', detail:'Completada', life:3000})
-              console.log(this.usuarios)
-              console.log(this.usuario)
-              for(let i=0;i<this.usuario.length;i++){
-                if(this.usuario[i].id == this.usuarios.id){
-                  console.log("dentro")
-                  this.usuario[i]=this.usuarios
-                  this.visible=false
-                }
-                this.visible=false
-              }
-            }, 1000)
-          },
-          error: (err) => {
-            console.log(err)
-            this.messageService.add({ severity:'error', summary: 'Actualizar usuario', detail: 'Error al actualizar el usuario, inténtelo de nuevo', life: 3000 });
-          }
-        })
-      }
-    }
-  }
+//   async guardar(b:Boolean){
+//     if(b){
+//       if(this.validaciones()){
+//          this.servicioUsers.usuariosPut(this.usuarios,this.id).subscribe({
+//           next: (data:any)=> {
+//             setTimeout(()=>{
+//               this.messageService.add({severity:'success', summary:'Actualizar usuario', detail:'Completada', life:3000})
+//               console.log(this.usuarios)
+//               console.log(this.usuario)
+//               for(let i=0;i<this.usuario.length;i++){
+//                 if(this.usuario[i].id == this.usuarios.id){
+//                   console.log("dentro")
+//                   this.usuario[i]=this.usuarios
+//                   this.visible=false
+//                 }
+//                 this.visible=false
+//               }
+//             }, 1000)
+//           },
+//           error: (err) => {
+//             console.log(err)
+//             this.messageService.add({ severity:'error', summary: 'Actualizar usuario', detail: 'Error al actualizar el usuario, inténtelo de nuevo', life: 3000 });
+//           }
+//         })
+//       }
+//     }
+//   }
 
-  showDialog(){
-    this.servicioUsers.usuarioGet(this.id!).subscribe({
+//   showDialog(){
+//     this.servicioUsers.usuarioGet(this.id!).subscribe({
       
-      next: (usu:Users) => {
-        this.us = usu
-        this.visible=true
-        this.usuarios.nombre = usu.nombre
-        this.usuarios.email = usu.email
-        this.usuarios.password = usu.password
-      },
-      error: (e) => {
-        console.log(e)
-      }
-    })
-  }
-}
+//       next: (usu:Users) => {
+//         this.us = usu
+//         this.visible=true
+//         this.usuarios.nombre = usu.nombre
+//         this.usuarios.email = usu.email
+//         this.usuarios.password = usu.password
+//       },
+//       error: (e) => {
+//         console.log(e)
+//       }
+//     })
+//   }
+// }
--- front/src/app/components/profile-icon/profile-icon.component.ts
+++ front/src/app/components/profile-icon/profile-icon.component.ts
@@ -1,46 +1,62 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges, input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges,Output, EventEmitter } from '@angular/core';
 import { MenuItem, MessageService } from 'primeng/api';
 
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { ToastModule } from 'primeng/toast';
 import { AuthService } from '../../services/auth.service';
 import { RouterLink } from '@angular/router';
-//Óscar
+import { UsersService } from '../../services/users.service';
+import { Users } from '../../interface/users';
+import { Subscription } from 'rxjs';
+import { DialogModule } from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
+import { ConfirmComponent } from '../confirm/confirm.component';
+import { ModificarRolesComponent } from '../modificar-roles/modificar-roles.component';
+//Óscar 
+// Raul-> Perfil
 // Laura -> añadir tus eventos
 @Component({
   selector: 'app-profile-icon',
   standalone: true,
-  imports: [ToastModule,SplitButtonModule],
+  imports: [ToastModule,SplitButtonModule,DialogModule,FormsModule,ConfirmComponent,ModificarRolesComponent],
   templateUrl: './profile-icon.component.html',
   styleUrl: './profile-icon.component.css',
   providers: [ MessageService,AuthService ]
 })
 export class ProfileIconComponent implements OnInit, OnChanges {
   @Input() roles:Array<string>=[]
-  items: MenuItem[];
+  items: MenuItem[]=[];
   @Input() nombre='usuario'
-  id:number = 0
-  constructor(private messageService: MessageService, private servicioAutenticacion:AuthService) {
-      this.items = [
-          {
-              label: 'Update',
-              icon: 'pi pi-refresh',
-              command: () => {
-                  this.update();
-              }
-          },
-          {
-              label: 'Delete',
-              icon: 'pi pi-times',
-              command: () => {
-                  this.delete();
-              }
-          },
-          { label: 'Angular.io', icon: 'pi pi-info', url: 'http://angular.io' },
-          { separator: true },
-          { label: 'Cerrar Sesion', icon: 'pi pi-power-off', routerLink: ['/installation'] }
-      ];
+  constructor(private messageService: MessageService, private servicioAutenticacion:AuthService,
+    private servicioUsers: UsersService
+    ) {
+
   }
+
+  value = ''
+  us!:Users
+  usuarios: Users = { 
+    id: 0, 
+    nombre: '', 
+    email: '', 
+    password: '' 
+  }
+  @Input() usuario?: any
+  @Input() id!: number
+  subscripcionUsuarios:Subscription = new Subscription();
+
+  @Input() visible: boolean = false
+
+  @Output() cerrarModal = new EventEmitter<void>();
+
+  @Input() tipo=0
+
+  estiloValidacionNombre = ''
+  estiloValidacionCorreo = ''
+  estiloValidacionContrasena = ''
+
+
+
 ngOnInit(): void {
 
  this.nombre=this.servicioAutenticacion.getName()
@@ -51,17 +67,8 @@ ngOnInit(): void {
 ngOnChanges(changes: SimpleChanges): void {
   
 }
-  save(severity: string) {
-      this.messageService.add({ severity: severity, summary: 'Success', detail: 'Data Saved' });
-  }
+  
 
-  update() {
-      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Data Updated' });
-  }
-
-  delete() {
-      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Data Deleted' });
-  }
   crearMenu(){
     let menu: MenuItem[]=[];
     for(const rol of this.roles   ){
@@ -91,5 +98,104 @@ ngOnChanges(changes: SimpleChanges): void {
      }
     })
     this.items=menu
+  }
+
+
+
+  validaciones():Boolean{
+
+    let validado = true
+    let nombreRegex = /^(?=.{3,15}$)[A-ZÁÉÍÓÚ][a-zñáéíóú]+([\s-][A-ZÁÉÍÓÚ][a-zñáéíóú]+)?$/
+    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+    if (this.usuarios.nombre){
+
+      var nombrePrueba = nombreRegex.test(this.usuarios.nombre)
+      
+      if (nombrePrueba){
+        this.estiloValidacionNombre
+      }else{
+        this.estiloValidacionNombre = 'ng-invalid ng-dirty'
+        validado = false
+        this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El nombre no puede contener caracteres especiales', life:3000})
+
+      }
+    }else{
+
+      this.estiloValidacionNombre = 'ng-invalid ng-dirty'
+      validado = false
+      this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El nombre es obligatorio', life:3000})
+
+    }
+
+    if (this.usuarios.email){
+
+      var emailPrueba = emailRegex.test(this.usuarios.email)
+      
+      if (emailPrueba){
+        this.estiloValidacionCorreo
+      }else{
+        this.estiloValidacionCorreo = 'ng-invalid ng-dirty'
+        validado = false
+        this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El correo tiene que tener el @', life:3000})
+
+      }
+    }else{
+
+      this.estiloValidacionCorreo = 'ng-invalid ng-dirty'
+      validado = false
+      this.messageService.add({severity: 'warn', summary:'Editar usuario', detail:'El correo es obligatorio', life:3000})
+
+    }
+    return validado
+  }
+
+
+  cerrar(): void {
+    this.cerrarModal.emit();
+  }
+
+  async guardar(b:Boolean){
+    if(b){
+      if(this.validaciones()){
+         this.servicioUsers.usuariosPut(this.usuarios,this.id).subscribe({
+          next: (data:any)=> {
+            setTimeout(()=>{
+              this.messageService.add({severity:'success', summary:'Actualizar usuario', detail:'Completada', life:3000})
+              console.log(this.usuarios)
+              console.log(this.usuario)
+              for(let i=0;i<this.usuario.length;i++){
+                if(this.usuario[i].id == this.usuarios.id){
+                  console.log("dentro")
+                  this.usuario[i]=this.usuarios
+                  this.visible=false
+                }
+                this.visible=false
+              }
+            }, 1000)
+          },
+          error: (err) => {
+            console.log(err)
+            this.messageService.add({ severity:'error', summary: 'Actualizar usuario', detail: 'Error al actualizar el usuario, inténtelo de nuevo', life: 3000 });
+          }
+        })
+      }
+    }
+  }
+
+  showDialog(){
+    this.servicioUsers.usuarioGet(this.id!).subscribe({
+      
+      next: (usu:Users) => {
+        this.us = usu
+        this.visible=true
+        this.usuarios.nombre = usu.nombre
+        this.usuarios.email = usu.email
+        this.usuarios.password = usu.password
+      },
+      error: (e) => {
+        console.log(e)
+      }
+    })
   }
 }
